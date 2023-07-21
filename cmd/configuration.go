963e2d7caa8aaf9807f06b15d1a3f8d26231ff93
Angela <angela@getdray.com>
2023-07-21 15:13:44 -0700

    Use DATABASE_URL for database connection

--- cmd/configuration.go
+++ cmd/configuration.go
@@ -1,5 +1,7 @@
 package cmd
 
+import "os"
+
 // These variables are meant to inserted as env vars
 // you can inject them through orchestrator secrets or command line args or OS env vars
 // Precedence: Command-line flags > Env vars > Default values.
@@ -23,20 +25,37 @@ type DatabaseConfig struct {
 	DatabaseConnectionPort string `arg:"env:DATABASE_CONNECTION_PORT"`
 }
 
-// DefaultConfiguration return dev configuration as default config
 func DefaultConfiguration() *Config {
 	return &Config{
-		Environment: "dev",
+		Environment: os.Getenv("ENVIRONMENT"),
 		ServerConfig: ServerConfig{
-			Name: "backend",
-			Port: "8080",
+			Name: os.Getenv("SERVER_NAME"),
+			Port: os.Getenv("SERVER_PORT"),
 		},
 		DatabaseConfig: DatabaseConfig{
-			DatabaseName:           "postgres",
-			DatabaseUsername:       "postgres",
-			DatabasePassword:       "postgres",
-			DatabaseConnectionUrl:  "localhost",
-			DatabaseConnectionPort: "5432",
+			DatabaseName:          os.Getenv("DATABASE_NAME"),
+			DatabaseUsername:      os.Getenv("DATABASE_USERNAME"),
+			DatabasePassword:      os.Getenv("DATABASE_PASSWORD"),
+			DatabaseConnectionUrl: os.Getenv("DATABASE_URL"),
+			//DatabaseConnectionPort: os.Getenv("DATABASE_CONNECTION_PORT"),
 		},
 	}
 }
+
+//// DefaultConfiguration return dev configuration as default config
+//func DefaultConfiguration() *Config {
+//	return &Config{
+//		Environment: "dev",
+//		ServerConfig: ServerConfig{
+//			Name: "backend",
+//			Port: "8080",
+//		},
+//		DatabaseConfig: DatabaseConfig{
+//			DatabaseName:          "postgres",
+//			DatabaseUsername:      "postgres",
+//			DatabasePassword:      "postgres",
+//			DatabaseConnectionUrl: "postgres://ehxxgxaamkwsaz:15acfc1d3ddc307e898673d17a669f2169913ef6467380439410f045e42f3e4d@ec2-52-0-79-72.compute-1.amazonaws.com:5432/d7mdhbr0m054dr",
+//			//DatabaseConnectionPort: "5432",
+//		},
+//	}
+//}
