f2d850077983e31db63741ffe4c951e8e5631639
angela purcell <angelapurcell@angelas-MBP-2.lan>
2024-02-10 12:47:14 -0800

    ADDED ERROR

--- handlers/certification_handler.go
+++ handlers/certification_handler.go
@@ -2,7 +2,8 @@ package handlers
 
 import (
 	"errors"
-    "fmt"
+	"fmt"
+
 	"github.com/Moral-Authority/backend/database"
 	"github.com/Moral-Authority/backend/graph/model"
 	"github.com/Moral-Authority/backend/models"
@@ -13,80 +14,80 @@ type CertificationService struct{}
 
 func (s CertificationService) AddNewCertification(request model.AddCertification, dbService database.CertificationDbService) (*model.Certification, error) {
 
-    cert := models.Certification{
-        Name:               null.StringFrom(*request.Name),
-        Logo:               null.StringFrom(*request.Logo),
-        Industry:           null.StringFrom(*request.Industry),
-        Certifier:          null.StringFrom(*request.Certifier),
-        CertifiesCompany: null.BoolFrom(*request.CertifiesCompany),
-        CertifiesProduct:  null.BoolFrom(*request.CertifiesProduct),
-        CertifiesProcess: null.BoolFrom(*request.CertifiesProcess),
-        CertifierContactID: null.StringFrom(*request.CertifierContactID), 
-        Audited:            null.BoolFrom(*request.Audited),
-        Auditor:            null.StringFrom(*request.Auditor),
-        Region:             null.StringFrom(*request.Region),
-        Qualifiers:         null.StringFrom(*request.Qualifiers),
-        Sources:            null.StringFrom(*request.Sources),
-    }
+	cert := models.Certification{
+		Name:               null.StringFrom(*request.Name),
+		Logo:               null.StringFrom(*request.Logo),
+		Industry:           null.StringFrom(*request.Industry),
+		Certifier:          null.StringFrom(*request.Certifier),
+		CertifiesCompany:   null.BoolFrom(*request.CertifiesCompany),
+		CertifiesProduct:   null.BoolFrom(*request.CertifiesProduct),
+		CertifiesProcess:   null.BoolFrom(*request.CertifiesProcess),
+		CertifierContactID: null.StringFrom(*request.CertifierContactID),
+		Audited:            null.BoolFrom(*request.Audited),
+		Auditor:            null.StringFrom(*request.Auditor),
+		Region:             null.StringFrom(*request.Region),
+		Qualifiers:         null.StringFrom(*request.Qualifiers),
+		Sources:            null.StringFrom(*request.Sources),
+	}
 
-    addedCert, err := dbService.AddNewCertification(cert)
-    if err != nil || addedCert == nil {
-        return nil, errors.New(fmt.Sprintf("unable to add cert to db, ERROR: %s", err))
-    }    
+	addedCert, err := dbService.AddNewCertification(cert)
+	if err != nil || addedCert == nil {
+		return nil, errors.New(fmt.Sprintf("unable to add cert to db, ERROR: %s", err))
+	}
 
-    return toCertificationResponse(*addedCert), nil
+	return toCertificationResponse(*addedCert), nil
 }
 
 func (s CertificationService) GetAllCertifications(dbService database.CertificationDbService) ([]*model.Certification, error) {
-    certs, err := dbService.GetAllCertifications()
-    if err != nil {
-        return nil, err
-    }
+	certs, err := dbService.GetAllCertifications()
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("unable to get certs from db, ERROR: %s", err))
+	}
 
-    var response []*model.Certification
-    for _, e := range certs {
-        cert := toCertificationResponse(e)
-        response = append(response, cert)
-    }
-    return response, nil
+	var response []*model.Certification
+	for _, e := range certs {
+		cert := toCertificationResponse(e)
+		response = append(response, cert)
+	}
+	return response, nil
 }
 
 func (s CertificationService) GetCertificationById(certId string, dbService database.CertificationDbService) (*model.Certification, error) {
 	cert, err := dbService.GetCertificationById(certId)
-    if err != nil || cert == nil {
-        return nil, errors.New(fmt.Sprintf("unable to get cert from db, ERROR: %s", err))
-    }   
+	if err != nil || cert == nil {
+		return nil, errors.New(fmt.Sprintf("unable to get cert from db, ERROR: %s", err))
+	}
 
 	return toCertificationResponse(*cert), nil
 }
 
 func (s CertificationService) UpdateCertification(request model.UpdateCertification, dbService database.CertificationDbService) (*model.Certification, error) {
-    // Retrieve the existing certification
-    cert, err := dbService.GetCertificationById(request.ID)
-    if err != nil || cert == nil {
-        return nil, errors.New(fmt.Sprintf("unable to get cert from db, ERROR: %s", err))
-    }   
+	// Retrieve the existing certification
+	cert, err := dbService.GetCertificationById(request.ID)
+	if err != nil || cert == nil {
+		return nil, errors.New(fmt.Sprintf("unable to get cert from db, ERROR: %s", err))
+	}
 
-    // Update the fields
-    cert.Name = null.StringFrom(*request.Name)
-    cert.Logo = null.StringFrom(*request.Logo)
-    cert.Industry = null.StringFrom(*request.Industry)
-    cert.Certifier = null.StringFrom(*request.Certifier)
-    cert.CertifiesCompany = null.BoolFrom(*request.CertifiesCompany)
-    cert.CertifiesProduct = null.BoolFrom(*request.CertifiesProduct)
-    // cert.CertifiesProcess = null.BoolFrom(*request.CertifiesProcess)
-    cert.CertifierContactID = null.StringFrom(*request.CertifierContactID)
-    cert.Audited = null.BoolFrom(*request.Audited)
-    cert.Auditor = null.StringFrom(*request.Auditor)
-    cert.Region = null.StringFrom(*request.Region)
-    cert.Qualifiers = null.StringFrom(*request.Qualifiers)
-    cert.Sources = null.StringFrom(*request.Sources)
+	// Update the fields
+	cert.Name = null.StringFrom(*request.Name)
+	cert.Logo = null.StringFrom(*request.Logo)
+	cert.Industry = null.StringFrom(*request.Industry)
+	cert.Certifier = null.StringFrom(*request.Certifier)
+	cert.CertifiesCompany = null.BoolFrom(*request.CertifiesCompany)
+	cert.CertifiesProduct = null.BoolFrom(*request.CertifiesProduct)
+	// cert.CertifiesProcess = null.BoolFrom(*request.CertifiesProcess)
+	cert.CertifierContactID = null.StringFrom(*request.CertifierContactID)
+	cert.Audited = null.BoolFrom(*request.Audited)
+	cert.Auditor = null.StringFrom(*request.Auditor)
+	cert.Region = null.StringFrom(*request.Region)
+	cert.Qualifiers = null.StringFrom(*request.Qualifiers)
+	cert.Sources = null.StringFrom(*request.Sources)
 
-    // Save the updated certification
-    updatedCert := dbService.UpdateCertification(*cert)
-    if updatedCert == nil {
-        return nil, errors.New("unable to update certificate in db")
-    }
+	// Save the updated certification
+	updatedCert := dbService.UpdateCertification(*cert)
+	if updatedCert == nil {
+		return nil, errors.New("unable to update certificate in db")
+	}
 
-    return toCertificationResponse(*updatedCert), nil
+	return toCertificationResponse(*updatedCert), nil
 }
